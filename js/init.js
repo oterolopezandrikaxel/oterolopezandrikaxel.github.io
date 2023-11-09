15aaa99b421e1ce5a63c8dddd876b4f9a6651228
oterolopezandrikaxel <150397389+oterolopezandrikaxel@users.noreply.github.com>
2023-11-10 00:05:18 +0100

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
- apiKey: "AIzaSyAx_Uryb-0QSxse6drmiJNBo5_FzJ_TiS8",
-  authDomain: "aaol-d60ec.firebaseapp.com",
-  projectId: "aaol-d60ec",
-  storageBucket: "aaol-d60ec.appspot.com",
-  messagingSenderId: "630477089204",
-  appId: "1:630477089204:web:426e60699f42845b946ae1",
-  measurementId: "G-PXCEXTK63P"
+   apiKey: "AIzaSyAx_Uryb-0QSxse6drmiJNBo5_FzJ_TiS8",
+    authDomain: "aaol-d60ec.firebaseapp.com",
+    projectId: "aaol-d60ec",
+    storageBucket: "aaol-d60ec.appspot.com",
+    messagingSenderId: "630477089204",
+    appId: "1:630477089204:web:426e60699f42845b946ae1",
+    measurementId: "G-PXCEXTK63P"
     });
